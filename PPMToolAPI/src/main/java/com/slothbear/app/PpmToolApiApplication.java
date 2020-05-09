c3142a6095d0cb73e8c51b50aafa602029515447
suryakv14@outlook.com <suryakv14@outlook.com>
2020-05-09 13:24:49 -0700

    Initial Project Commit

---
+++ PPMToolAPI/src/main/java/com/slothbear/app/PpmToolApiApplication.java
@@ -0,0 +1,13 @@
+package com.slothbear.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApiApplication.class, args);
+	}
+
+}
